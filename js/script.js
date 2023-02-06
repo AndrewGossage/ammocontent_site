a359ebb15a8353fcaedd5c4531330bbed4083e55
Andrew Gossage <gossachfront@gmail.com>
2023-02-06 10:17:11 -0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+( function( $ ) {
+$( document ).ready(function() {
+$('#cssmenu').prepend('<div id="menu-button">Menu</div>');
+	$('#cssmenu #menu-button').on('click', function(){
+		var menu = $(this).next('ul');
+		if (menu.hasClass('open')) {
+			menu.removeClass('open');
+		}
+		else {
+			menu.addClass('open');
+		}
+	});
+});
+} )( jQuery );
+
+
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	});
